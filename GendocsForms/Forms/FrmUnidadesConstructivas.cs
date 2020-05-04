49a7d18b93a0f68ae6165b3a31cb66167b66f464
Alex Mota <aletxu_18@hotmail.com>
2020-05-04 11:04:57 +0200

    FrmUnidadesConstructivas

---
+++ GendocsForms/Forms/FrmUnidadesConstructivas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GendocsForms
+{
+    public partial class FrmUnidadesConstructivas : Form
+    {
+        public FrmUnidadesConstructivas()
+        {
+            InitializeComponent();
+        }
+    }
+}
