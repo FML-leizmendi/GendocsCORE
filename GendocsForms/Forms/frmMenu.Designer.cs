9f48d661872c65c9751c7d9f02f6905701dff37c
leizmendi <aletxu_18@hotmail.com>
2020-06-30 12:30:43 +0200

    Login WPF

--- GendocsForms/Forms/frmMenu.Designer.cs
+++ GendocsForms/Forms/frmMenu.Designer.cs
@@ -274,7 +274,6 @@
             this.button1.TabIndex = 15;
             this.button1.Text = "PRUEBAS";
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FrmMenu
             // 
